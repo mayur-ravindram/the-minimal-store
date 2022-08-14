85c9cfde76e2b9658cb677f6bf7b1b392ad6be9a
mayur-ravindram <mayurravindram@gmail.com>
2022-08-14 22:20:32 +0530

    draft# adding unit test for controller

--- backEnd/src/test/java/com/learner/theArtifact/controller/ControllerTest.java
+++ backEnd/src/test/java/com/learner/theArtifact/controller/ControllerTest.java
@@ -1,14 +1,44 @@
 package com.learner.theArtifact.controller;
 
+import com.learner.theArtifact.model.Product;
+import com.learner.theArtifact.service.MongoProductService;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.Arrays;
+
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 public class ControllerTest {
-    @LocalServerPort
-    private int port = 8080;
 
-    public void testController() {
+    @Autowired
+    private MockMvc mvc;
+
+    @MockBean
+    private MongoProductService mongoProductService;
+
+    @Test
+    public void testController() throws Exception {
+
+        given(this.mongoProductService.list())
+                .willReturn(Arrays.asList(new Product("","","",0.0,"","","")));
+
+        this.mvc.perform(get("/mongo/list")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
 
     }
 }
+
